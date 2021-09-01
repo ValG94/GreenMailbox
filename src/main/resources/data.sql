cabbc7c0416d2d81059e0fba5fdd5c6bdd618b9a
Valéry <valerygarrec@gmail.com>
2021-09-01 10:26:51 +0200

    dernieres modifs VG

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,22 +3,22 @@ VALUES (1, 'Valery'),
        (2, 'Clara');
 
 insert into challenge (id, level, like_challenge, name_challenge, description, img_url, date_creation)
-VALUES (1, 200, 5, 'Green day', 'nettoyer sa boite mail et la rendre green',
-        'https://banner2.cleanpng.com/20190716/zfq/kisspng-world-map-globe-vector-graphics-the-importance-of-listening-listening-for-a-change-5d2e7df5c8cce4.0819082415633279898225.jpg',
+VALUES (1, 200, 5, 'Clean Green', 'Découvrir le Clean green pour rendre sa boite mail plus green',
+        'https://static6.depositphotos.com/1107614/598/v/950/depositphotos_5985020-stock-illustration-green-earth.jpg',
         '16-07-2021'),
-       (2, 150, 4, 'Green day', 'nettoyer sa boite mail et la rendre green',
-        'https://banner2.cleanpng.com/20190716/zfq/kisspng-world-map-globe-vector-graphics-the-importance-of-listening-listening-for-a-change-5d2e7df5c8cce4.0819082415633279898225.jpg',
+       (2, 150, 4, 'Box Green', 'Comment optimiser sa boite mail ?',
+        'http://www.clker.com/cliparts/E/m/W/q/A/j/green-mailbox-hi.png',
         '16-07-2021'),
-       (3, 100, 2, 'Green day', 'nettoyer sa boite mail et la rendre green',
-        'https://banner2.cleanpng.com/20190716/zfq/kisspng-world-map-globe-vector-graphics-the-importance-of-listening-listening-for-a-change-5d2e7df5c8cce4.0819082415633279898225.jpg',
+       (3, 100, 2, 'Green day', 'Journée pour cleaner son bureau et le rendre green',
+        'https://www.greenofficemovement.org/wp-content/uploads/2020/06/Green-Office-Bologna-Logo.png',
         '16-07-2021'),
-       (4, 50, 4, 'Green day', 'nettoyer sa boite mail et la rendre green',
-        'https://banner2.cleanpng.com/20190716/zfq/kisspng-world-map-globe-vector-graphics-the-importance-of-listening-listening-for-a-change-5d2e7df5c8cce4.0819082415633279898225.jpg',
+       (4, 50, 4, 'Green Idea', 'Brainstorming green pour trouver les idées qui nous rendrons encore plus éco responsable',
+        'https://www.quickanddirtytips.com/sites/default/files/images/8213/Eco.jpg',
         '16-07-2021');
 
 insert into content_challenge (id, answers, questions, challenge_id)
-VALUES (1, 'True', '"Un mail est-il plus lourd vs publication Teams ?"', 1)
+VALUES (1, 'True', '"Un mail est-il plus lourd vs publication Teams ?"', 1);
 
-insert into resource (url_resource) VALUES
-    (https://support.microsoft.com/fr-fr/office/organiser-votre-bo%C3%AEte-de-r%C3%A9ception-9aea8ad7-883d-459b-a3ea-ae20f06cfb32),
-    (https://www.greenit.fr/2018/09/11/reduire-limpact-de-e-mails/)
+/*insert into resource (id, url_resource)
+VALUES (1, 'https://support.microsoft.com/fr-fr/office/organiser-votre-bo%C3%AEte-de-r%C3%A9ception-9aea8ad7-883d-459b-a3ea-ae20f06cfb32'),
+    (2, 'https://www.greenit.fr/2018/09/11/reduire-limpact-de-e-mails/');*/
